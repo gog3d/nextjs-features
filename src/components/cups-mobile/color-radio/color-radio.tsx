4095e770f583e68bbaba8d398ed0a7162840d2c9
gog3d <gluhovog3d@gmail.com>
2023-05-29 23:36:38 +0300

    add ts regroupe2

--- src/components/cups-mobile/color-radio/color-radio.tsx
+++ src/components/cups-mobile/color-radio/color-radio.tsx
@@ -7,7 +7,7 @@ const ColorRadio = ({checked, color}) => {
   
   return (
     <label>
-      <input className={styles['color__radio_input']} type="radio" name="color" checked/>
+      <input className={styles['color__radio_input']} type="radio" name="color"/>
         <div className={styles['color__button-container']}>
           <div className={styles['color__button']} style={style}></div>
         </div>
