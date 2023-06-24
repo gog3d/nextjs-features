502dc85c84f0c2fd641b23ac5e94847e8838bb46
gog3d <gluhovog3d@gmail.com>
2023-06-24 19:34:31 +0300

    add zoom 0.05

--- src/components/cups/constructor-canvas/constructor-canvas.tsx
+++ src/components/cups/constructor-canvas/constructor-canvas.tsx
@@ -13,13 +13,13 @@ const ConstructorCanvas: FC<IConstructorCanvasProps> = ({children, zoom, setZoom
   
   const zoomIn = useCallback(()=>{
     if(zoom && setZoom){
-      setZoom(zoom + 0.1);
+      setZoom(zoom + 0.05);
     }
   }, [zoom, setZoom]);
 
   const zoomOut = useCallback(()=>{
     if(setZoom && zoom && zoom > 0.2) {
-      setZoom(zoom - 0.1);
+      setZoom(zoom - 0.05);
     }
   }, [zoom, setZoom]);
 
