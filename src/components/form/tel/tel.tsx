3813e421822ae6ad4ca99c63b76069f7139d847e
gog3d <gluhovog3d@gmail.com>
2023-12-23 16:39:03 +0300

    add tel delete number

--- src/components/form/tel/tel.tsx
+++ src/components/form/tel/tel.tsx
@@ -16,19 +16,20 @@ const Tel: FC = () => {
     dispatch(formActions.tel(e.target.value));
   };
 
+//  const pattern = new RegExp("^\\+[1-9]{1}[0-9]{0,2}-[2-9]{1}[0-9]{2}-[2-9]{1}[0-9]{2}-[0-9]{4}$");
+
 
   return (
     <div  className={styles['input-wrapper']}>
       <input 
         value={value}
         className={styles['input']}
-        type={'number'}
+        type={'tel'}
         placeholder={'Телефон'}
-        pattern={'[\+]\d{1}\s[\(]\d{3}[\)]\s\d{3}[\-]\d{2}[\-]\d{2}'}
         onChange={handler}
       />
       <span className={styles['form__error']}>
-        {'Это поле должно содержать телефон в формате +71234567890'}
+        {'Это поле должно содержать телефон в формате +7(123)456-78-90'}
       </span>
     </div>
   );
@@ -37,3 +38,9 @@ const Tel: FC = () => {
 export default Tel;
 //        pattern={'[\+]\d{1}\s[\(]\d{3}[\)]\s\d{3}[\-]\d{2}[\-]\d{2}'}
 //        {'Это поле должно содержать телефон в формате +7 (123) 456-78-90'}
+//        pattern={'[\+]\d{1}\s[\(]\d{3}[\)]\s\d{3}[\-]\d{2}[\-]\d{2}'}
+/*
+      <span className={styles['form__error']}>
+        {'Это поле должно содержать телефон в формате +7(123)456-78-90'}
+      </span>
+  */
